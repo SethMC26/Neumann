d01025976f34a06cc4dd08cc8f7aadd0841f83f8
Seth Holtzman <holtzmans@merrimack.edu>
2025-09-26 11:32:05 -0400

    refactor: render function supports changing axis and headers only include selectable headers

--- src/VonViz/Model/AppModel.swift
+++ src/VonViz/Model/AppModel.swift
@@ -8,6 +8,7 @@ enum Axis {
     case z
 }
 
+/// Row of data for use in Chart3D
 struct Row : Identifiable {
     let id: Int
     let x: Double
@@ -19,28 +20,27 @@ struct Row : Identifiable {
 class AppModel: ObservableObject{
     /// limit to data that can be displayed
     private let DISPLAY_LIMIT: Int = 100
-    // Data that the user has imported, nil is none has been imported yet
+    /// Data that the user has imported, nil is none has been imported yet
     private var data: DataFrame? = nil
-    // Data that is being displayed currented(sliced dataframe from data)
+    /// Data that is being displayed currented(sliced dataframe from data)
     private var currDataDisplayed: DataFrame? = nil
     
     /// Map of axis and header associated with axis to display
     private var axes: [Axis: String] = [ : ]
-    /// All headers of data
-    private var headers: [String] = []
-    
+    /// All headers of columns that can be changed
+    @Published var headers: [String] = []
+    /// Rows of dataset updated when new file loaded or axis to display is changed
     @Published var rows: [Row] = []
-    
-    // TO:DO add visualization
-    //private var vis : Object
-    
     
     /// Set an Axis to be associated with a particular header
     /// - Parameters:
     ///   - axisToSet: Axis to set
     ///   - header: Header to set
-    func setAxis(axisToSet: Axis, header: String) {
+    /// - Throws:
+    ///      - AppError from render functions
+    func setAxis(axisToSet: Axis, header: String) throws {
         axes[axisToSet] = header
+        try render()
     }
     
     /// Get the Header associated with a certain Axis
@@ -54,47 +54,41 @@ class AppModel: ObservableObject{
     /// the inputting of datasets to the visualization.
     /// - Parameter url: URL of local file to load
     func ingestFile(file: URL) throws {
-        
+        //remove old data
         data = try DataFrame(contentsOfCSVFile: file)
         currDataDisplayed = nil
         rows = []
         axes.removeAll()
         
-        // get all the headers for each columns if one is nil
-        headers = data?.columns.map {$0.name} ?? []
-        
-        //basic temporary code will need to update header setting later
-        
-        // check if we have less than three columns error out
-        if headers.count < 3 {
-            throw AppError.notEnoughColumns
-        }
-        
+        //Should not error out here but include this check to please the compiler
         guard let df = data else { throw AppError.noLoadedDataset }
+        
+        // names of the columns with numbers that a user can select
         var col_names: [String] = []
         
         //attempt to automatically set axes
         for col in df.columns {
             let type = col.wrappedElementType
+            // add column if it is a number type
+            // @note in the future we could include int8, int16, int32 types but im not sure if this is necessary
             if type == Int.self || type == Float.self || type == Double.self {
                 col_names.append(col.name)
             }
             
-            if col_names.count == 3 {
-                axes = [Axis.x: col_names[0], Axis.y: col_names[1], Axis.z: col_names[2]]
-                self.currDataDisplayed = df[[col_names[0], col_names[1], col_names[2]]]
-                break
-            }
         }
+        
+        // if there are less than 3 columns with numbers in them dataset cannot be loaded
+        if col_names.count < 3 {
+            throw AppError.notEnoughColumns
+        }
+        
+        headers = col_names
+        //set displayed data to first three applicable columns
+        axes = [Axis.x: col_names[0], Axis.y: col_names[1], Axis.z: col_names[2]]
+        self.currDataDisplayed = df[[col_names[0], col_names[1], col_names[2]]]
         
         try render()
         
-    }
-    
-    /// Gets all of the headers of `self.data`
-    /// - Returns: Headers of `self.data`
-    func getHeaders() -> [String]? {
-        return headers
     }
     
     
@@ -107,16 +101,12 @@ class AppModel: ObservableObject{
         }
         
         //get headers for x y and z axis throw an error if axis has not been set yet (cannot render visualization)
-        guard let xLabel = axes[.x] else {
-            throw AppError.xNotSet
-        }
-        
-        guard let yLabel = axes[.y] else {
-            throw AppError.yNotSet
-        }
-        
-        guard let zLabel = axes[.z] else {
-            throw AppError.zNotSet
+        guard
+            let xLabel = axes[.x],
+            let yLabel = axes[.y],
+            let zLabel = axes[.z]
+        else {
+            throw AppError.headerNotRecongized
         }
         
         var newRows: [Row] = []
@@ -134,18 +124,19 @@ class AppModel: ObservableObject{
             }
         }
         
-        print(newRows)
         //change state of row at the end once all rows loaded
         //UI is waiting for a state change on rows so we only want to change once its ready
         rows = newRows
-       
+
     }
     
+    /// chatGPT generated function to return a number value as a double
     private func asDouble(_ value: Any?) throws -> Double {
         switch value {
         case let d as Double: return d
         case let i as Int:    return Double(i)
         case let f as Float:  return Double(f)
+        //throw internalStateError we should only be calling this function on values we know will be an Int Double or Float
         default: throw AppError.internalStateError
         }
     }
@@ -154,9 +145,7 @@ class AppModel: ObservableObject{
 enum AppError: Error {
     case noLoadedDataset
     case notEnoughColumns
-    case xNotSet
-    case yNotSet
-    case zNotSet
+    case headerNotRecongized
     case internalStateError
 }
 
