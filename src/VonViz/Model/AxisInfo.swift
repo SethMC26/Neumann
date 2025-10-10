68d51bc0f074c36994cc4b73fbf41208d3068ceb
Seth Holtzman <holtzmans@merrimack.edu>
2025-10-10 16:47:43 -0400

    refact: throw error when bad column passed in with logging statements and added API docs

--- src/VonViz/Model/AxisInfo.swift
+++ src/VonViz/Model/AxisInfo.swift
@@ -12,12 +12,20 @@ struct AxisInfo {
     var min: Double = -50
     var steps: Double = 1.0
     
+    /// Gets the domain of this function
+    /// - Returns: Closed range between min...max
     func getDomain() throws -> ClosedRange<Double> {
         return min...max
     }
     
+    /// Set the value of this struct, automatically sets min and max based on the input column
+    /// - Parameters:
+    ///   - header: Name of column
+    ///   - column: Column with values to set
     mutating func setValues(header: String, column: AnyColumn) throws {
-        let values = column.compactMap { value -> Double? in
+        // column could be any so we will cast it to doubles
+        // This function should never have the ability to have non-numerical columns passed into it 
+        let values = try? column.compactMap { value -> Double? in
             switch value {
             case let v as Double:
                 return v
@@ -26,15 +34,17 @@ struct AxisInfo {
             case let v as Int:
                 return Double(v)
             default:
-                return nil
+                Log.Model.fault("Got value in column that was not numerical - should have already filtered these columns out")
+                throw AppError.internalStateError
             }
         }
         
         //I hate swift coding patterns
         guard
-            let minVal = values.min(),
-            let maxVal = values.max()
+            let minVal = values?.min(),
+            let maxVal = values?.max()
         else {
+            Log.Model.fault("Could not get min or max value - should never get here")
             throw AppError.internalStateError
         }
 
